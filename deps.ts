255482fe5cb4acc7dc4e7ad8bf837e3b73c6d6ce
Siddharth <siddharth.muscat@gmail.com>
2021-09-07 12:36:19 +0530

    Add fs dependency

--- deps.ts
+++ deps.ts
@@ -1,7 +1,8 @@
 import {parse, Args, ArgParsingOptions} from "https://deno.land/std@0.106.0/flags/mod.ts";
 import {parse as parseToml, stringify as stringifyToml} from "https://deno.land/std@0.106.0/encoding/toml.ts";
+import {existsSync} from "https://deno.land/std@0.106.0/fs/mod.ts";
 
 export {parse};
 export type {Args, ArgParsingOptions};
-
 export {parseToml, stringifyToml};
+export {existsSync}
