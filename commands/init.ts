808f533a4124a1d60732ea7e4818924d08e926ef
Siddharth <siddharth.muscat@gmail.com>
2021-09-07 12:37:08 +0530

    Create init command

--- commands/init.ts
+++ commands/init.ts
@@ -1,8 +1,16 @@
-import {Args} from '../deps.ts';
+import {Args, existsSync} from '../deps.ts';
 
 /*
  * Initialize a new postr directory
  */
 export function handleInit(argv: Args) {
+	const configExists = existsSync('postr.toml');
 
+	if (configExists && !argv.force) {
+		console.error('fatal: config already exists. Run with --force to reinitialize');
+		Deno.exit(1);
+	}
+
+	Deno.writeTextFile('postr.toml', '# Todo');
+	console.log((configExists ? 'Rei' : 'I') + 'nitialized postr in ' + Deno.cwd())
 }
--- index.ts
+++ index.ts
@@ -4,7 +4,11 @@ import {handleInit} from './commands/init.ts';
 import {handleNew} from './commands/new.ts';
 import {handleRefresh} from './commands/refresh.ts';
 
-parseArgs(Deno.args)
-	.command('init', handleInit)
-	.command('new', handleNew)
-	.command('refresh', handleRefresh);
+parseArgs(Deno.args, {
+		boolean: ['force', 'f'],
+		alias: {
+			f: 'force',
+		}
+	}).command('init', handleInit)
+		.command('new', handleNew)
+		.command('refresh', handleRefresh);
