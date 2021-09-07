d6bc60a1d231918c36f5be3d593251d08304314f
Siddharth <siddharth.muscat@gmail.com>
2021-09-07 12:01:45 +0530

    Add toml deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,7 @@
 import {parse, Args, ArgParsingOptions} from "https://deno.land/std@0.106.0/flags/mod.ts";
+import {parse as parseToml, stringify as stringifyToml} from "https://deno.land/std@0.106.0/encoding/toml.ts";
 
 export {parse};
 export type {Args, ArgParsingOptions};
+
+export {parseToml, stringifyToml};
