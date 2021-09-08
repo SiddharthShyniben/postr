e45adf1eb095c8b482b7aeee61689e09c3b87ed3
Siddharth <siddharth.muscat@gmail.com>
2021-09-08 12:36:33 +0530

    Drop the schema thing

--- utils/help-messages.ts
+++ utils/help-messages.ts
@@ -3,14 +3,12 @@ export const helpMessages: {[command: string]: any} = {
 
 Available commands:
   *init*:              Initialize a new postr directory with some default config
-  *new* _<schema>_:      Generate new _<schema>_.
-                     _<schema>_ can either be the built in _post_ or
-                     any others exposed by a plugin
+  *new*:                Generate new post.
   *refresh*:           The main command to publish/unpublish/update posts and run plugins.
-  *help* _<command>+:    Show help for _<command>_. If _<command>_ is not provided,
-                     This message will be shown`,
+  *help* _<command>_:    Show help for _<command>_. If _<command>_ is not provided,
+                     This message will be shown.`,
 	init: `*postr init:* Initializes the current directory as a postr directory`,
-	new: `*postr new* _<schema>:_ Generates a new _<schema>_. A schema is a template for a post`,
+	new: `*postr new*: Generates a new post.`,
 	refresh: `*postr refresh:* This is an all in one command for:
 
                1. Updating any changed posts
