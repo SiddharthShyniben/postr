1ec459bdd1af81e7b389a62d49a9b46ed9922e8a
Siddharth <siddharth.muscat@gmail.com>
2021-09-08 13:18:32 +0530

    Add new command

--- commands/new.ts
+++ commands/new.ts
@@ -1,8 +1,24 @@
-import {Args} from '../deps.ts';
+import {Args, ensureFile, ensureDir, existsSync, slugify} from '../deps.ts';
 
 /*
  * Create a new post
  */
 export function handleNew(argv: Args) {
+	const name = argv._[1];
 
+	if (!existsSync('postr.toml')) {
+		console.error('fatal: not a postr directory');
+		Deno.exit(1);
+	}
+
+	if (name) {
+		const slug = slugify(name.toString());
+
+		ensureFile(`${slug}/post.md`);
+		ensureDir(`${slug}/assets/images`);
+		Deno.writeTextFile(`${slug}/post.md`, `---\ntitle: ${name}---\n\n_What amazing things will you write today?_`)
+	} else {
+		console.error('fatal: no name was provided');
+		Deno.exit(1);
+	}
 }
