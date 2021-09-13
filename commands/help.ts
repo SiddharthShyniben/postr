2dd7673cf3f1774bee0f0fc450b6c13c47f568bb
Siddharth <siddharth.muscat@gmail.com>
2021-09-13 15:55:11 +0530

    Revert "Better help messages"
    This reverts commit 806e1f96178e862740e3a8132efb8d4111dd112b.

--- commands/help.ts
+++ commands/help.ts
@@ -6,21 +6,12 @@ import {hl, fail} from '../utils/ui.ts'
  * Initialize a new postr directory
  */
 export function handleHelp({_}: Args) {
-	const key: string | number | undefined = _[1];
+	const key: string | number | undefined = _[1] ?? '_default';
+	const message = helpMessages[key.toString()];
 
-	if (!key) {
-		console.log(hl(helpMessages._default));
-		return;
-	}
-
-	if (!(key in helpMessages)) fail('command not found')
+	if (!message) fail('command not found');
 
 	console.log(
-		hl(
-			// Hacky typing
-			getKeyValue(key.toString())(helpMessages)
-		)
+		hl(message)
 	);
 }
-
-const getKeyValue = (key: string) => (obj: Record<string, any>) => obj[key];
