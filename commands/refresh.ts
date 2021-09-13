4ace4a96b79c5923a3040aa53cf7353f9b537d20
Siddharth <siddharth.muscat@gmail.com>
2021-09-13 15:17:43 +0530

    Basic refresh command

--- commands/refresh.ts
+++ commands/refresh.ts
@@ -1,6 +1,53 @@
-import {Args} from '../deps.ts';
+import {Args, expandGlob, existsSync, parseToml} from '../deps.ts';
+import {fail} from '../utils/ui.ts';
 
 /*
  * Refresh (publish all due posts, unpublish all other, run plugins)
  */
-export function handleRefresh(argv: Args) {}
+export async function handleRefresh(_argv: Args) {
+	if (!existsSync('postr.toml')) fail('not a postr directory')
+
+	const posts = expandGlob('posts/*');
+	for await (const post of posts) {
+		if (!post.isDirectory) {
+			console.warn('warning: non-folder found in posts directory')
+			continue;
+		}
+
+		const {parsedFrontMatter, contents} = extractFrontMatter(await Deno.readTextFile(post.path + '/post.md'));
+
+		let {adapters}= parsedFrontMatter;
+		adapters ??= [];
+
+		if (!Array.isArray(adapters)) fail('adapters is not an array');
+		else adapters.forEach(async (adapter: string) => {
+			let {adapterPlugins} = parseToml(await Deno.readTextFile('postr.toml'));
+			adapterPlugins ??= {};
+
+			if (!isObject(adapterPlugins)) fail('adapterPlugins in the configuration is not an object');
+
+			const adapterPath = (adapterPlugins as any)[adapter];
+
+			if (!adapterPath) {
+				console.warn('warn: an adapter was set' +
+					'but the corresponding plugin was not configured in `postr.toml`. Skipping');
+				return;
+			}
+
+			import(adapterPath.path)
+				.then(async module => await module.publish(contents, parsedFrontMatter, adapterPath.config))
+				.catch(err => fail(`could not run adapter because of ${err.name}: ${err.message}`));
+		});
+	}
+}
+
+function extractFrontMatter(contents: string) {
+	const frontMatterRegex = /^\+{3}$([\s\S]+?)^\+{3}$/gim;
+
+	const frontMatter = frontMatterRegex.exec(contents) ?? [];
+	const parsedFrontMatter = parseToml(frontMatter[1] ?? '');
+
+	return {parsedFrontMatter, contents: contents.replace(frontMatterRegex, '')}
+}
+
+const isObject = (thing: any) => Object.prototype.toString.call(thing) !== '[object Object]'
