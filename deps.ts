028c76a6fe3ee7e2aa321c8ebd577c50b4040c80
Siddharth <siddharth.muscat@gmail.com>
2021-09-13 15:16:07 +0530

    Add more fs deps

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
 import {parse, Args, ArgParsingOptions} from 'https://deno.land/std@0.106.0/flags/mod.ts';
 import {parse as parseToml, stringify as stringifyToml} from 'https://deno.land/std@0.106.0/encoding/toml.ts';
-import {existsSync, ensureFile, ensureDir} from 'https://deno.land/std@0.106.0/fs/mod.ts';
+import {existsSync, ensureFile, ensureDir, expandGlob} from 'https://deno.land/std@0.106.0/fs/mod.ts';
 import {slugify} from 'https://deno.land/x/slugify@0.3.0/mod.ts';
 
 export {parse};
 export type {Args, ArgParsingOptions};
 export {parseToml, stringifyToml};
-export {existsSync, ensureFile, ensureDir};
+export {existsSync, ensureFile, ensureDir, expandGlob};
 export {slugify};
 
 // Not a dep
