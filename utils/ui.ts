721bb48302285295f3c518a12cb13e17016ebbc2
Siddharth <siddharth.muscat@gmail.com>
2021-09-13 15:15:49 +0530

    UI utilities

---
+++ utils/ui.ts
@@ -0,0 +1,11 @@
+export const fail = (why: string) => {
+	console.error(hl(`_fatal:_ ${why}`));
+	Deno.exit(1);
+}
+
+export const highlight1 = (string: string) => `\u001B[32;1m${string}\u001B[0m`;
+export const highlight2 = (string: string) => `\u001B[31;1m${string}\u001B[0m`;
+
+export const hl = (string: string) => string
+	.replace(/\*(.*?)\*/gim, (_match: string, $1: string) => highlight1($1))
+	.replace(/_(.*?)_/gim, (_match: string, $1: string) => highlight2($1));
