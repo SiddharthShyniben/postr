26ade6490757392dca5ed8028f6fa0bd03e3834e
Siddharth <siddharth.muscat@gmail.com>
2021-09-19 19:36:01 +0530

    Simple post chcking

---
+++ utils/post-checker.ts
@@ -0,0 +1,9 @@
+import {Post} from '../post.ts';
+import {nanoid} from './deps.ts';
+
+export const fillPost = (post: Partial<Post>) => Object.assign({draft: true, title: 'Untitled', id: nanoid()}, post)
+export const shouldPostBeChecked = (post: Post) => fillPost(post).draft;
+
+export const isPostModified = (post: Post) => {
+
+};
