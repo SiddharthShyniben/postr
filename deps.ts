95e9d2c61a97a517c368b6b26ad6304cfc80e14c
Siddharth <siddharth.muscat@gmail.com>
2021-09-21 15:29:57 +0530

    Better deps

--- deps.ts
+++ deps.ts
@@ -1,15 +1,9 @@
-import {parse, Args, ArgParsingOptions} from 'https://deno.land/std@0.106.0/flags/mod.ts';
-import {parse as parseToml, stringify as stringifyToml} from 'https://deno.land/std@0.106.0/encoding/toml.ts';
-import {existsSync, ensureFile, ensureDir, expandGlob} from 'https://deno.land/std@0.106.0/fs/mod.ts';
-import {slugify} from 'https://deno.land/x/slugify@0.3.0/mod.ts';
-import {nanoid} from 'https://deno.land/x/nanoid@v3.0.0/mod.ts';
-
-export {parse};
-export type {Args, ArgParsingOptions};
-export {parseToml, stringifyToml};
-export {existsSync, ensureFile, ensureDir, expandGlob};
-export {slugify};
-export {nanoid};
+export {parse} from 'https://deno.land/std@0.106.0/flags/mod.ts';
+export type {Args, ArgParsingOptions} from 'https://deno.land/std@0.106.0/flags/mod.ts'
+export {parse as parseToml, stringify as stringifyToml} from 'https://deno.land/std@0.106.0/encoding/toml.ts';
+export {existsSync, ensureFile, ensureDir, expandGlob} from 'https://deno.land/std@0.106.0/fs/mod.ts';
+export {slugify} from 'https://deno.land/x/slugify@0.3.0/mod.ts';
+export {nanoid} from 'https://deno.land/x/nanoid@v3.0.0/mod.ts';
 
 // Not a dep
 export const VERSION = '0.0.0';
