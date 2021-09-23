819f7d29486f465e272982a3d045ef28845a3022
Siddharth <siddharth.muscat@gmail.com>
2021-09-23 10:54:44 +0530

    Add db deps

--- deps.ts
+++ deps.ts
@@ -4,6 +4,7 @@ export {parse as parseToml, stringify as stringifyToml} from 'https://deno.land/
 export {existsSync, ensureFile, ensureDir, expandGlob} from 'https://deno.land/std@0.106.0/fs/mod.ts';
 export {slugify} from 'https://deno.land/x/slugify@0.3.0/mod.ts';
 export {nanoid} from 'https://deno.land/x/nanoid@v3.0.0/mod.ts';
+export {Database} from 'https://deno.land/x/aloedb@0.9.0/mod.ts'
 
 // Not a dep
 export const VERSION = '0.0.0';
