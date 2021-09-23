c262cb482cc32a01b9076e34ac3461ef8d8ee8f7
Siddharth <siddharth.muscat@gmail.com>
2021-09-23 10:54:34 +0530

    ID mapping db

---
+++ utils/db.ts
@@ -0,0 +1,13 @@
+import {Database} from '../deps.ts';
+
+interface IDMapping {
+	local: string;
+	remote: string;
+	platform: string;
+}
+
+const idMappingDB = new Database<IDMapping>('./.postr/id-db.json');
+
+export async function addMapping(local: string, remote: string, platform: string) {
+	await idMappingDB.insertOne({local, remote, platform});
+}
--- utils/post-checker.ts
+++ utils/post-checker.ts
@@ -8,11 +8,15 @@ type postStatus = 'draftPublish' |
 	'delete' |
 	'noop';
 
-export const fillPost = (post: Partial<Post>): Post => Object.assign({draft: true, title: 'Untitled', id: nanoid()}, post) as Post;
+export const fillPost = (post: Partial<Post>): Post => Object.assign({
+	draft: true, title: 'Untitled', id: nanoid()
+}, post) as Post;
 export const shouldPostBeChecked = (post: Partial<Post>): boolean => fillPost(post).draft;
 
 export const getActionForPost = (post: Partial<Post>): postStatus => {
 	post = fillPost(post);
+
+	console.log(post);
 
 	if (post.modified) {
 		if (post.draft) return 'draftPublish';
